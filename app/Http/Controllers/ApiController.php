bb00b1263d3d19191d4a0ba32551099a04438633
Emma <emma@else.co.il>
2022-08-04 16:03:53 +0600

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -15,7 +15,7 @@ class ApiController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth.custom:api', ['except' => ['login', 'jwt']]);
+        $this->middleware('auth.custom:api', ['except' => ['login', 'jwt_dec']]);
     }
 
     protected function guard()

cab413a9c45f0dd72fc41a705354094417ee5846
Sheikh Shourov <emma@else.co.il>
2022-08-19 01:00:32 +0600

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -35,7 +35,7 @@ class ApiController extends Controller
         );
         if ($validator->fails()) {
             $status = false;
-            $errors = $validator->errors();
+            $errors = $validator->errors()->getMessages();
             return response()->json(compact('status', 'errors'));
         }
         $credentials = array('email' => $request->email, 'password' => $request->password);

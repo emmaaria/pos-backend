c877bb92620d9a8b19fcd70705fc52288f2890ea
Emma <emma@else.co.il>
2022-08-18 15:38:34 +0600

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1288,9 +1288,9 @@ class ApiController extends Controller
         $name = $request->name;
         if (empty($name)) {
             $products = DB::table('products')
-                ->select('products.name AS product_name','products.product_id AS product_id', DB::raw('SUM(purchase_items.quantity) as totalPurchase'), DB::raw('SUM(invoice_items.quantity) as totalSale'), DB::raw('SUM(totalPurchase - totalSale) as stock'))
-                ->leftJoin('invoice_items', 'invoice_items.product_id', '=', 'products.product_id')
-                ->leftJoin('purchase_items', 'purchase_items.product_id', '=', 'products.product_id')
+                ->select('products.name AS product_name','products.product_id AS product_id', DB::raw('SUM(purchase_items.quantity) as totalPurchaseQuantity'), DB::raw('SUM(invoice_items.quantity) as totalSaleQuantity'), DB::raw('SUM(totalPurchaseQuantity - totalSaleQuantity) as stock'))
+                ->rightJoin('invoice_items', 'invoice_items.product_id', '=', 'products.product_id')
+                ->rightJoin('purchase_items', 'purchase_items.product_id', '=', 'products.product_id')
                 ->groupBy('products.product_id')
                 ->paginate(50);
             $status = true;

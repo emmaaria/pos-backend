dd4a052a5b3c95782197249a2d1152e37e8dd6d0
Else Dev <admin@elkabetz.me>
2022-11-21 00:14:19 +0600

    New

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2129,6 +2129,7 @@ class ApiController extends Controller
                 DB::table('nagad_transactions')->where('reference_no', "inv-$id")->where('company_id', $companyId)->delete();
                 DB::table('bkash_transactions')->where('reference_no', "inv-$id")->where('company_id', $companyId)->delete();
                 DB::table('cash_books')->where('reference_no', "inv-$id")->where('company_id', $companyId)->delete();
+                DB::table('bank_ledgers ')->where('reference_no', "inv-$id")->where('company_id', $companyId)->delete();
                 $status = true;
                 $message = 'Invoice deleted';
                 return response()->json(compact('status', 'message'));

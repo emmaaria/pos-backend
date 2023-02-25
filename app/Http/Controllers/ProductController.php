c025c6f36283c22e3476af4bdfa8c4778e181e87
emmaaria <emma@else.co.il>
2023-02-25 23:56:09 +0600

    New

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -321,12 +321,13 @@ class ProductController extends Controller
                 return response()->json(compact('status', 'products'));
             } else {
                 $products = DB::table('products')
-                    ->selectRaw("products.name AS name, products.product_id, products.price,(select sum(quantity) from invoice_items where product_id= `products`.`product_id`) as 'sale',(select sum(quantity) from purchase_items where product_id= `products`.`product_id`) as 'purchase'")
+                    ->selectRaw("products.name AS name, products.product_id, products.price,(select sum(quantity) from invoice_items where product_id= `products`.`product_id`) + (select sum(quantity) from sale_return_items where product_id= `products`.`product_id`) as 'sale',(select sum(quantity) from purchase_items where product_id= `products`.`product_id`) as 'purchase'")
                     ->where('products.company_id', $companyId)
                     ->where('products.name', 'like', '%' . $name . '%')
                     ->orWhere('products.product_id', 'like', '%' . $name . '%')
                     ->leftJoin('invoice_items', 'invoice_items.product_id', '=', 'products.product_id')
                     ->leftJoin('purchase_items', 'purchase_items.product_id', '=', 'products.product_id')
+                    ->leftJoin('sale_return_items', 'sale_return_items.product_id', '=', 'products.product_id')
                     ->groupBy('products.product_id', 'products.name')
                     ->paginate(50);
                 $status = true;

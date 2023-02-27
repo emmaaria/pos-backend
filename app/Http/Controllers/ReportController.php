4bf031d99144f61d3edcdbc16402f1cba044cfa6
emmaaria <emma@else.co.il>
2023-02-27 18:06:45 +0600

    New

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Exception;
 
@@ -71,8 +72,47 @@ class ReportController extends Controller
                 ->where('purchases.company_id', $companyId)
                 ->where('suppliers.company_id', $companyId)
                 ->leftJoin('suppliers', 'suppliers.id', '=', 'purchases.supplier_id')
+                ->where('purchases.date', '>=', $request->startDate)
+                ->where('purchases.date', '<=', $request->endDate)
                 ->orderBy('purchases.id', 'desc')
                 ->get();
+            $status = true;
+            return response()->json(compact('status', 'data'));
+        } else {
+            $status = false;
+            $errors = 'You are not authorized';
+            return response()->json(compact('status', 'errors'));
+        }
+    }
+
+    public function customerLedger(Request $request)
+    {
+        $companyId = $this->getCompanyId();
+        if ($companyId) {
+            $validator = Validator::make($request->all(),
+                [
+                    'customer_id' => 'required'
+                ]
+            );
+            if ($validator->fails()) {
+                $status = false;
+                $errors = $validator->errors();
+                return response()->json(compact('status', 'errors'));
+            }
+            $query = DB::table('customers')
+                ->select('customers.name', 'purchases.purchase_id', 'purchases.amount', 'purchases.comment', 'purchases.id', 'purchases.date')
+                ->where('customers.id', $request->customer_id)
+                ->where('customers.company_id', $companyId)
+                ->where('customer_ledgers.company_id', $companyId)
+                ->leftJoin('customer_ledgers', 'customer_ledgers.customer_id', '=', 'customers.id')
+                ->orderBy('customer_ledgers.date', 'desc');
+            if (!empty($request->startDate)){
+                $query->where('customer_ledgers.date', '>=', $request->startDate);
+            }
+            if (!empty($request->endDate)){
+                $query->where('customer_ledgers.date', '<=', $request->endDate);
+            }
+            $data = $query->get();
             $status = true;
             return response()->json(compact('status', 'data'));
         } else {
--- routes/report.php
+++ routes/report.php
@@ -12,4 +12,5 @@ use Illuminate\Support\Facades\Route;
 Route::group(['middleware' => 'api'], function ($router) {
     Route::post('/report/sales', [ReportController::class, 'sales']);
     Route::post('/report/purchase', [ReportController::class, 'purchase']);
+    Route::post('/report/customer/ledger', [ReportController::class, 'customerLedger']);
 });

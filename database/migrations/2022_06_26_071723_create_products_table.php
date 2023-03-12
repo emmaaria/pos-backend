0423cd17065582dcddaa9c1a9401f50d49c22578
emmaaria <emma@else.co.il>
2023-03-12 18:05:57 +0600

    New

--- database/migrations/2022_06_26_071723_create_products_table.php
+++ database/migrations/2022_06_26_071723_create_products_table.php
@@ -17,11 +17,11 @@ class CreateProductsTable extends Migration
             $table->id();
             $table->string('name');
             $table->index('product_id')->change();
-            $table->index('category')->nullable();
+            $table->index('category')->nullable()->change();
             $table->string('unit')->nullable();
             $table->string('price')->nullable();
             $table->string('purchase_price')->nullable();
-            $table->index('company_id');
+            $table->index('company_id')->change();
             $table->string('weight')->default(0);
             $table->timestamps();
         });

2d07958d9efb522770ea43334b7a20ce9410554d
emmaaria <emma@else.co.il>
2023-04-10 00:22:02 +0600

    New

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -6,6 +6,7 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Skycoder\InvoiceNumberGenerator\InvoiceNumberGeneratorService;
 use Tymon\JWTAuth\Facades\JWTAuth;
@@ -257,6 +258,7 @@ class InvoiceController extends Controller
                             if (isset($productDiscountedAmounts[$i]) && $productDiscountedAmounts[$i] !== '') {
                                 $prDisAmount = $productDiscountedAmounts[$i];
                             }
+                            Log::debug($productDiscountedAmounts[$i]);
 
                             if ($quantity > 0) {
                                 $ttl = $quantity * $price;

26beccda33bde1cb4aae417271e2fcdf10002003
emmaaria <emma@else.co.il>
2023-06-12 01:20:37 +0600

    New

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -342,6 +342,7 @@ class ReportController extends Controller
                     'products.name',
                     'products.weight',
                     DB::raw('SUM(invoice_items.quantity) as qty'),
+                    DB::raw('(SUM(invoice_items.quantity) * SUM(invoice_items.weight)) as total_weight'),
                 )
                 ->where('invoices.company_id', $companyId)
                 ->where('invoices.customer_id', $request->customer)

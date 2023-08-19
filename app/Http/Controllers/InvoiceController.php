fef9f41080d773147d4e8f35b25a8401a0484606
emmaaria <emma@else.co.il>
2023-08-19 13:26:09 +0600

    New update

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -6,6 +6,7 @@ use App\Models\AveragePurchasePrice;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Skycoder\InvoiceNumberGenerator\InvoiceNumberGeneratorService;
 use Tymon\JWTAuth\Facades\JWTAuth;
@@ -802,6 +803,7 @@ class InvoiceController extends Controller
                 } catch (\Exception $e) {
                     $status = false;
                     $errors = $e;
+                    Log::error($e);
                     return response()->json(compact('status', 'errors'));
                 }
             } else {
